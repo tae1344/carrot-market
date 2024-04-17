eb1a64b350cdbd43cfedb98d10e44220330ed488
tae1344 <xodus1344@gmail.com>
2024-04-17 23:03:50 +0900

    server action + useFormStatus 훅 사용해 form action을 쉽게 처리한다

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,16 +3,20 @@ import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  const handleForm = async (data: FormData) => {
+    "use server";
+  };
+
   return (
     <div className={"flex flex-col gap-10 px-6 py-8"}>
       <div className={"flex flex-col gap-2 *:font-medium"}>
         <h1 className={"text-2xl"}>안녕하세요</h1>
         <h2 className={"text-xl"}>Log in with email and password.</h2>
       </div>
-      <form className={"flex flex-col gap-3"} action="">
-        <FormInput type={"email"} placeholder={"Email"} required={true} errors={[]} />
-        <FormInput type={"password"} placeholder={"Password"} required={true} errors={[]} />
-        <FormButton loading={false} text={"Create account"} />
+      <form action={handleForm} className={"flex flex-col gap-3"}>
+        <FormInput name={"email"} type={"email"} placeholder={"Email"} required={true} errors={[]} />
+        <FormInput name={"password"} type={"password"} placeholder={"Password"} required={true} errors={[]} />
+        <FormButton text={"Create account"} />
       </form>
       <SocialLogin />
     </div>
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,12 +3,14 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
-export default function FormInput({ type, placeholder, required, errors }: FormInputProps) {
+export default function FormInput({ type, placeholder, required, errors, name }: FormInputProps) {
   return (
     <div className={"flex flex-col gap-2"}>
       <input
+        name={name}
         className={
           "h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500"
         }
