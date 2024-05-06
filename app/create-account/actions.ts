d12c66afa4c8c9158adecbca30d289d5245d83f1
tae1344 <xodus1344@gmail.com>
2024-05-06 22:39:07 +0900

    zod로 비밀번호가 같은지 체크한다

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,19 +2,26 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z
-    .string({
-      invalid_type_error: "Username must be a string",
-      required_error: "Where is my username?",
-    })
-    .min(3, "Way too short!")
-    .max(10, "That is too long!")
-    .optional(),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkPassword = ({ password, confirm_password }: { password: string; confirm_password: string }) =>
+  password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string",
+        required_error: "Where is my username?",
+      })
+      .min(3, "Way too short!")
+      .max(10, "That is too long!"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPassword, {
+    message: "Both password should be the same!",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(pervState: any, formData: FormData) {
   const data = {
